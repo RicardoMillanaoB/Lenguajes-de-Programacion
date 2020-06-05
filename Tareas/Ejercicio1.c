5f12e8359961a4786a6eabf120c3b41509b8dcc2
Ricardo Millanao <r.millanao02@ufromail.cl>
2020-06-05 00:20:51 -0400

    Update Ejercicio1.c

--- Tareas/Ejercicio1.c
+++ Tareas/Ejercicio1.c
@@ -1,16 +1,21 @@
-//Calculode area y perimetro
+//Calculo de descuento sobre precio por teclado
 
 #include<stdio.h>
 
 void main(){
-	int ladoA;
-	int ladoB;
-	printf("\nIngrese el lado A: ");
-	scanf("%d",&ladoA);
-	printf("\nIngrese el lado B ");
-	scanf("%d",&ladoB);
-	int area = ladoA*ladoB;
-	int perimetro = (ladoA*2)+(ladoB*2);
-	printf("\nSu área:%d ",area);
-	printf("\nSu perimetro:%d ",perimetro);
+		
+		int precioNeto;
+		int descuento;
+		printf("\nIngrese el precio: ");
+		scanf("%d",&precioNeto);
+		printf("\nIngrese el descuento (en porcentaje): ");
+		scanf("%d",&descuento);	
+	
+		double  precioFinal = precioNeto-((precioNeto*0.01)*descuento);
+	
+		printf("\nPrecio Neto: %d",precioNeto);
+		printf("\ncon descuento de: %d ",descuento);
+	
+		printf("\nPrecio Total: %f",precioFinal);
+	
 }
